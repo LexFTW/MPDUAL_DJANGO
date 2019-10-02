1769f4019c57c6e182ca4e1f3497061e4da91305
super <lexmengual@gmail.com>
2019-10-02 20:06:05 +0200

    02102019_200545

---
+++ polls/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Vote(models.Model):
+    answer_id = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    data_time = models.DateTimeField()
+    location = models.CharField(max_length=200)
